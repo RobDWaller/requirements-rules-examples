250719c7e17a9599c3a15cc8dbd109583651abdc
RobDWaller <rdwaller1984@googlemail.com>
2018-11-19 10:11:10 +0000

    Added invalid validate tests for validator class

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -28,3 +28,78 @@ class TestValidator(unittest.TestCase):
         result = validator.validate()
 
         self.assertTrue(result['result'])
+
+    def test_validate_invalid_firstname(self):
+
+        validator = Validator(
+            'firstname!',
+            'lastname',
+            'rob@test.com',
+            '18/11/2018',
+            'on'
+        )
+
+        result = validator.validate()
+
+        self.assertFalse(result['result'])
+        self.assertEqual(result['messages'][0], 'Please fill in a valid first name only alpha numeric characters, spaces and dashes allowed.')
+
+    def test_validate_invalid_lastname(self):
+
+        validator = Validator(
+            'firstname',
+            'lastname!',
+            'rob@test.com',
+            '18/11/2018',
+            'on'
+        )
+
+        result = validator.validate()
+
+        self.assertFalse(result['result'])
+        self.assertEqual(result['messages'][0], 'Please fill in a valid last name only alpha numeric characters, spaces and dashes allowed.')
+
+    def test_validate_invalid_email(self):
+
+        validator = Validator(
+            'firstname',
+            'lastname',
+            'robtest.com',
+            '18/11/2018',
+            'on'
+        )
+
+        result = validator.validate()
+
+        self.assertFalse(result['result'])
+        self.assertEqual(result['messages'][0], 'Please fill in a valid email.')
+
+    def test_validate_invalid_dob(self):
+
+        validator = Validator(
+            'firstname',
+            'lastname',
+            'rob@test.com',
+            '2018/11/18',
+            'on'
+        )
+
+        result = validator.validate()
+
+        self.assertFalse(result['result'])
+        self.assertEqual(result['messages'][0], 'Please fill in a valid date of birth, format day/month/year, after 1900 and before or equal to today.')
+
+    def test_validate_invalid_tscs(self):
+
+        validator = Validator(
+            'firstname',
+            'lastname',
+            'rob@test.com',
+            '18/11/2018',
+            ''
+        )
+
+        result = validator.validate()
+
+        self.assertFalse(result['result'])
+        self.assertEqual(result['messages'][0], 'Please agree to our terms and conditions and privacy policy.')

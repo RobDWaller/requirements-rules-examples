c4d20ee3bff1a17521d687faba7790e9d8456d4a
RobDWaller <rdwaller1984@googlemail.com>
2018-11-19 13:24:10 +0000

    Got form working initially on front end and base template and success template.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from form.src.validator import Validator
 
 APP = Flask(__name__)
 
@@ -6,6 +7,23 @@ APP = Flask(__name__)
 def form():
 
     if request.method == 'POST':
-        return render_template('form.html', form = request.form)
 
-    return render_template('form.html')
+        validator = Validator(
+            request.form.get('firstname', ''),
+            request.form.get('lastname', ''),
+            request.form.get('email', ''),
+            request.form.get('dob', ''),
+            request.form.get('tac', '')
+        )
+
+        result = validator.validate()
+
+        if result['result'] == False:
+            data = {'form': request.form, 'messages': result['messages']}
+            return render_template('form.html', data=data)
+
+        data = {'form': request.form}
+        return render_template('success.html', data=data)
+
+    data = {'form': '', 'messages': ''}
+    return render_template('form.html', data=data)
